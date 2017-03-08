34221d4ca01f12aa77911f7d121411bad18add86
unknown <>
2017-03-08 15:45:07 +0800

    plot histogram

--- utils/dataset.py
+++ utils/dataset.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import xml.etree.ElementTree as ET
 from future.builtins import str
+from collections import defaultdict
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -14,7 +15,6 @@ class Log(object):
     def __init__(self, file=None):
         self.events = 0
         self.cases = []
-        self.acivities =  set()
         self.global_trace = set('case_duration')
         self.global_event = set(['event_duration', 'activity_id'])
         self.__log_from_xes(file)
@@ -50,12 +50,13 @@ class Log(object):
                             event[s.attrib['key']] = s.attrib['value']
                         event['activity_id'] = ' '.join([event['concept:name'], event['lifecycle:transition']])
                         events.append(event)
+                        self.events += 1
                     else:#case context
                         case[feature.attrib['key']] = feature.attrib['value']
                 events.sort(key=lambda e:e['time:timestamp'])
                 case['events'] = events
                 if case.get('endDate'):
-                    case['case_duration'] = (dates.datestr2num(case['startDate']) - dates.datestr2num(case['endDate']))
+                    case['case_duration'] = (dates.datestr2num(case['endDate']) - dates.datestr2num(case['startDate']))
                 else:
                     case['case_duration'] = None
                 self.cases.append(case)
@@ -109,18 +110,22 @@ class Log(object):
         #fig.show()
         fig.savefig("../resource/rev_time_chart.png")
 
-    def statistics(self):
-        if not self.acivities:
-            for trace in self.cases:
-                for event in trace.get('events'):
-                    self.acivities.add(event.get('activity_id'))
-                    self.events += 1
+    def get_unique_activities(self):
+        activities = set()
+        for trace in self.cases:
+            for event in trace.get('events'):
+                activities.add(event.get('activity_id'))
+        return activities
 
+    def statistics(self):
+        activities = self.get_unique_activities()
         all_cases = len(self.cases)
         all_events = self.events
+        completed_cases = 0
         longest_trace = 0
         shortest_trace = 999
         for t in self.cases:
+            completed_cases += 1 if t.get('caseStatus') == 'G' else 0
             l = len(t.get('events'))
             if l > longest_trace:
                 longest_trace = l
@@ -128,14 +133,64 @@ class Log(object):
                 shortest_trace = l
 
         print('Number of cases: ', all_cases)
-        print('Number of activities: ', len(self.acivities))
+        print('Number of completed cases: ', completed_cases)
+        print('Number of activities: ', len(activities))
         print('Number of events: ', all_events)
         print('Length of longest case: ', longest_trace)
         print('Length of shortest case: ', shortest_trace)
         print('Average length of case: ', '{0:.1f}'.format(all_events/all_cases))
+
+    def activity_frequencies(self, case_count=True):
+        '''
+        Return a dicitionary mapping each activity to the number of occurances
+        in the log.
+        [case_count]If True, count the number of cases in which the activity appears,
+        not the total occurances(each activity may appears multiple times per case).
+        '''
+        act_freq = defaultdict(int)
+        for case in self.cases:
+            trace = [e['activity_id'] for e in case.get('events')]
+            if case_count:
+                trace = set(trace)
+            for act in trace:
+                act_freq[act] += 1
+        return act_freq
+
+    def case_duration_histogram(self):
+        '''
+        Return a list of tuples(x, y) where x is the case identifier and y is
+        its time duration
+        '''
+        hist = []
+        for case in self.cases:
+            if not case.get('caseStatus') == 'G':
+                continue
+            if case.get('case_duration') == None:
+                continue
+            hist.append((case.get('concept:name'), case.get('case_duration')))
+        return hist
+
+    def plot_histogram(self, hist, xlabel='', ylabel='', title=''):
+        x, y = zip(*hist)
+        xpos = np.arange(len(x))
+        ax = plt.axes()
+        ax.set_xticklabels(x)
+        plt.bar(xpos, y)
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        plt.title(title)
+        plt.autoscale()
+        plt.savefig('../resource/' + title.replace(' ', '_') + '.png')
+        plt.show()
+
 
 if __name__ == "__main__":
      log = Log("../resource/BPIC15_3.xes.xml")
      #log.abs_time_chart()
      #log.rel_time_chart()
      log.statistics()
+     log.plot_histogram(log.case_duration_histogram(), xlabel='case: concept:name',
+                        ylabel='case_duration(days)', title='case duration distribute')
+     log.plot_histogram(log.activity_frequencies().items(), xlabel='activity',
+                        ylabel='frequency', title='activity frequency distribute')
+
